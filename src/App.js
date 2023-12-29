1b02bef64ed2fdb8c8df92de3ebdfb44f8e83498
sandesh-ghimire <ghimiresandesh45@gmail.com>
2023-12-29 22:49:09 +0530

    search feature for json file.

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// src/App.js
+// import React, { useState } from 'react';
+import './App.css';
+import SearchPage from './Search/SearchBox';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+     
+        <SearchPage />
+      </header>
+    </div>
+  );
+}
+
+export default App;
